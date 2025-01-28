ec4e801e52e8de25cea40383a5659dcd3ad41824
rohit <facets.raveendran@mercedes-benz.com>
2025-01-29 02:03:53 +0530

    remove az for simplicity

--- vpc/main.tf
+++ vpc/main.tf
@@ -6,7 +6,6 @@ resource "aws_subnet" "this" {
   count             = 1
   vpc_id            = aws_vpc.this.id
   cidr_block        = cidrsubnet(var.instance.spec.cidr, 8, count.index)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
 locals {
